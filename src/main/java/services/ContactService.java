440ff101a182df59a8feadb112d299f796a072fa
ALEXSANDRO FERNANDES SARTORI ROLIANO <ALU0810510@uniritter.local>
2019-05-18 16:30:17 -0300

    Teste

--- src/main/java/services/ContactService.java
+++ src/main/java/services/ContactService.java
@@ -1,5 +1,33 @@
 package services;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import models.Contact;
+import repositories.ContactRepository;
+
 public class ContactService {
 
+	@Autowired
+	private ContactRepository contactRepository;
+
+	public Optional<Contact> findById(Long id) {
+		return contactRepository.findById(id);
+	}
+
+	public List<Contact> findAll() {
+		return contactRepository.findAll();
+	}
+
+	public Contact save(Contact contact) {
+		return contactRepository.save(contact);
+	}
+
+	public void deleteById(long id) {
+		contactRepository.deleteById(id);
+	}
+
+	public void deleteAll() {
+		contactRepository.deleteAll();
+	}
 }
